3849c1ff59027447615fa2b4371f03c96bf2fa7b
JeetSarkar <sarkarindrajeet32@gmail.com>
2023-08-29 16:45:20 +0530

    push mcq's apllication1

--- themes/dashboard/views/layouts/student.blade.php
+++ themes/dashboard/views/layouts/student.blade.php
@@ -100,7 +100,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="{{ url('student/dashboard')}}" class="brand-link">
                 <span class="brand-text font-weight-light">Online Examination</span>
             </a>
 
